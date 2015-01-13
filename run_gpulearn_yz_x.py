65c4f76e469ade055b627c6c7bae2c416659fc5e
zhenxuan00 <610841656@qq.com>
2015-01-13 13:59:20 +0800

    New dataset

--- run_gpulearn_yz_x.py
+++ run_gpulearn_yz_x.py
@@ -1,18 +1,10 @@
 import gpulearn_yz_x
-import sys
+import sys, os
 
-if sys.argv[1] == 'svhn':
-    n_hidden = [500,500]
-    if len(sys.argv) == 4:
-        n_hidden = [int(sys.argv[2])]*int(sys.argv[3])
-    gpulearn_yz_x.main(dataset='svhn', n_z=300, n_hidden=n_hidden, seed=0, gfx=True)
-
-elif sys.argv[1] == 'mnist':
-    n_hidden = (500,500)
-    if len(sys.argv) >= 4:
-        n_hidden = [int(sys.argv[2])]*int(sys.argv[3])
-    n_z = 50
-    if len(sys.argv) >= 5:
-        n_z = int(sys.argv[4])
-    gpulearn_yz_x.main(dataset='mnist', n_z=n_z, n_hidden=n_hidden, seed=0, gfx=True)
-raise Exception("Unknown dataset")
+n_hidden = (500,500)
+if len(sys.argv) > 2:
+  n_hidden = tuple([int(x) for x in sys.argv[2:]])
+nz=500
+if os.environ.has_key('nz'):
+  nz = int(os.environ['nz'])
+gpulearn_yz_x.main(dataset=sys.argv[1], n_z=nz, n_hidden=n_hidden, seed=0, gfx=True)
